274ab2ec99657062337e20adb37280b8b3a58fd6
Braynner Silva <braynnert31@gmail.com>
2025-03-26 17:55:48 -0500

    initial proyect express-ejs

---
+++ config.js
@@ -0,0 +1,10 @@
+export const config = {
+  user: 'movilidad',
+  password: 'movilidad',
+  database: 'pruebaejs',
+  host: 'localhost',
+  port: 5432,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+};
